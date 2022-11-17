990d057010e3036edb638102aaf36b56c7a0b217
Eran Krauss <erankrauss1@gmail.com>
2022-11-17 12:45:24 +0200

    update readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# How to run the project
+
+1. Server - NodeJS
+    1. Enter the 'server' folder. ```cd server```
+    2. Run ```node ./index.js```
+
+
+    
+2. Client - React
+    1. Enter the 'client' folder. ```cd client```
+    2. Run ```npm run start```
--- client/src/screen/home/HomeScreen.js
+++ client/src/screen/home/HomeScreen.js
@@ -1,62 +1,68 @@
-import React, {useContext, useEffect, useState} from "react";
-import {ApiContext} from "../../App";
-import {ApplicationsTable} from "./ApplicationsTable";
-import {Box, Container, Divider, TextField} from "@mui/material";
+import React, { useContext, useEffect, useState } from "react";
+import { ApiContext } from "../../App";
+import { ApplicationsTable } from "./ApplicationsTable";
+import { Box, Container, Divider, TextField } from "@mui/material";
 import Title from "../../layout/title/Title";
-import './HomeScreen.css';
-import SearchIcon from '@mui/icons-material/Search';
-import {debounce} from "lodash";
+import "./HomeScreen.css";
+import SearchIcon from "@mui/icons-material/Search";
+import { debounce } from "lodash";
 
 export const HomeScreen = () => {
-    const [data, setData] = useState(undefined);
-    const [filteredData, setFilteredData] = useState(undefined);
-    const [showSearch, setShowSearch] = useState(false);
-    const api = useContext(ApiContext);
+  const [data, setData] = useState(undefined);
+  const [filteredData, setFilteredData] = useState(undefined);
+  const [showSearch, setShowSearch] = useState(false);
+  const api = useContext(ApiContext);
 
-    useEffect(() => {
-        api.getApplications()
-            .then(res => {
-                setData(res);
-                setFilteredData(res);
-            })
-            .catch(err => console.error('HomeScreen  >  useEffect  >  err details: ', err.message))
-    }, []);
+  useEffect(() => {
+    api
+      .getApplications()
+      .then((res) => {
+        setData(res);
+        setFilteredData(res);
+      })
+      .catch((err) =>
+        console.error("HomeScreen  >  useEffect  >  err details: ", err.message)
+      );
+  }, []);
 
-    const handleTextChange = (event) => {
-        event.preventDefault();
-        const txt = event.target.value;
-        const newData = data.filter(currData => {
-            const {id, firstName, lastName} = currData;
-            return id.toString().indexOf(txt) !== -1 || firstName.indexOf(txt) !== -1 || lastName.indexOf(txt) !== -1;
-        });
-        setFilteredData(newData)
-    }
+  const handleTextChange = (event) => {
+    event.preventDefault();
+    const txt = event.target.value.toString().toLowerCase();
+    const newData = data.filter((currData) => {
+      const { id, firstName, lastName } = currData;
+      return (
+        id.toString().toLowerCase().indexOf(txt) !== -1 ||
+        firstName.toLowerCase().indexOf(txt) !== -1 ||
+        lastName.toLowerCase().indexOf(txt) !== -1
+      );
+    });
+    setFilteredData(newData);
+  };
 
-    const handleTextChangeDebounce = debounce(handleTextChange, 300);
+  const handleTextChangeDebounce = debounce(handleTextChange, 300);
 
-
-    return (
-      <Container className={"container"}>
-        <div>
-          <Title title={"Eran Krauss Solution For JIFITI"} />
-          <Divider />
-        </div>
-        <div className="search">
-          {showSearch ? (
-            <TextField
-              id="outlined-basic"
-              variant="outlined"
-              fullWidth
-              label="Search"
-              onChange={handleTextChangeDebounce}
-            />
-          ) : (
-            <SearchIcon onClick={() => setShowSearch(true)} />
-          )}
-        </div>
-        <Box className={"table"}>
-          <ApplicationsTable data={filteredData} />
-        </Box>
-      </Container>
-    );
-}
+  return (
+    <Container className={"container"}>
+      <div>
+        <Title title={"Eran Krauss Solution For JIFITI"} />
+        <Divider />
+      </div>
+      <div className="search">
+        {showSearch ? (
+          <TextField
+            id="outlined-basic"
+            variant="outlined"
+            fullWidth
+            label="Search"
+            onChange={handleTextChangeDebounce}
+          />
+        ) : (
+          <SearchIcon onClick={() => setShowSearch(true)} />
+        )}
+      </div>
+      <Box className={"table"}>
+        <ApplicationsTable data={filteredData} />
+      </Box>
+    </Container>
+  );
+};
